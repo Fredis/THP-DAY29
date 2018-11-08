2458afd365126e8b2cc2e51e25c4637ea0ce7e13
Fredis <fredericstojicevic@gmail.com>
2018-11-08 17:10:44 +0100

    Add the URL used for scrapping in front-end.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   
   def index
+  	@url = 'https://coinmarketcap.com/all/views/all/'
     if Currency.first == nil
-  	  service_scrapping = ServiceScrapping.new('https://coinmarketcap.com/all/views/all/')
+  	  service_scrapping = ServiceScrapping.new(@url)
       service_scrapping.perform
     end 
     @currency = Currency.find(params[:currency_id]) unless params[:currency_id] == nil 
