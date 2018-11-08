880a851ae557d7f6fe2fca3b74a8cd95ed2fda00
Fredis <fredericstojicevic@gmail.com>
2018-11-08 14:42:51 +0100

    Beginning of form_for.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,16 @@ class HomeController < ApplicationController
   
   def index
 
-  	service_scrapping = ServiceScrapping.new('https://coinmarketcap.com/all/views/all/')
-  	service_scrapping.perform
+  	#service_scrapping = ServiceScrapping.new('https://coinmarketcap.com/all/views/all/')
+  	#service_scrapping.perform
+  end
 
+  def currency_price_display
+
+  	render 'index'
+		puts "/////"
+		@currency_name = Currency.find(params[:currency_id])
+		puts @currency_name.currency_name
   end
 
 end
